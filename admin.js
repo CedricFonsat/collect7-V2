85997744c1b7b451e013597a3a909df73344e4cb
cedricfonsat <79462084+CedricFonsat@users.noreply.github.com>
2023-03-23 11:15:10 +0100

    views

--- admin.js
+++ admin.js
@@ -1,6 +1,7 @@
 import AdminJS from 'adminjs';
 import AdminJSMongoose from '@adminjs/mongoose';
 import AdminJSExpress from '@adminjs/express';
+import express from 'express'
 
 // Importez le modèle Mongoose que vous souhaitez gérer avec AdminJS
 import userModel from './src/model/userModel.js';
@@ -13,14 +14,53 @@ AdminJS.registerAdapter(AdminJSMongoose);
 
 // Connexion à la base de données MongoDB avec Mongoose
 
+const usersNavigation = {
+    name: 'Admin',
+    icon: 'admin',
+  }
 
 // Créez une instance d'AdminJS en spécifiant les modèles à gérer et les options de configuration
 const adminJs = new AdminJS({
   // Spécifiez les modèles à gérer
-  resources: [userModel,cardModel,collectionModel],
+
+  resources: [
+    {
+    resource: userModel,
+    options: {
+         listProperties: ['username', 'email'],
+        filterProperties: ['username', 'email'],
+        editProperties: ['username', 'email', 'password'],
+       // showProperties: ['id', 'name', 'bio', 'createdAt'],
+       navigation: usersNavigation,
+      },
+  },{
+    resource: cardModel,
+    options: {
+         listProperties: ['name', 'price', 'collections'],
+        filterProperties: ['name', 'price','collections'],
+        editProperties: ['name', 'price', 'price', 'collections'],
+       // showProperties: ['id', 'name', 'bio', 'createdAt'],
+       navigation: usersNavigation,
+      },
+  },{
+    resource: collectionModel,
+    options: {
+         listProperties: ['name', 'category', 'description'],
+        filterProperties: ['name', 'category','description'],
+        editProperties: ['name', 'category', 'description'],
+       // showProperties: ['id', 'name', 'bio', 'createdAt'],
+       navigation: usersNavigation,
+      },
+  },
+],
   // ... d'autres options de configuration
+  locale: {
+    language: 'de'
+  },
 });
 
+
 const adminRoute = AdminJSExpress.buildRouter(adminJs);
+
 
 export default adminRoute;
